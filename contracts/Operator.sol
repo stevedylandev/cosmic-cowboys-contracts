4fae5c79ce9c29f9c78a9b7a836ce0afe97aa50e
Steve <stevedsimkins@gmail.com>
2023-10-09 13:37:52 -0400

    fixed TBA

---
+++ contracts/Operator.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./CosmicCowboy.sol";
+import "./SpaceGrub.sol";
+import "./RocketFuel.sol";
+import "./GoldenCorn.sol";
--- test/Test.js
+++ test/Test.js
@@ -1,9 +1,7 @@
-const { expect } = require("chai");
-
 // Import the ethers library
 const { ethers } = require("hardhat");
 
-async function runTests() {
+async function CosmicCoyboys() {
   let contract;
   let owner;
   let addr1;
@@ -28,4 +26,50 @@ async function runTests() {
   console.log(tokenURI);
 };
 
-runTests()
+async function GoldenCorn() {
+  let contract;
+  let owner;
+  let addr1;
+  let addr2;
+
+  // Get the signers from ethers
+  [owner, addr1, addr2] = await ethers.getSigners();
+
+  // Deploy the contract
+  const Contract = await ethers.getContractFactory("GoldenCorn");
+  contract = await Contract.deploy(owner.address);
+  const contractAddress = await contract.getAddress()
+  console.log("Contract deployed to address:", contractAddress);
+
+  // Mint tokens
+  await contract.mint(addr1.address, 100);
+  console.log("Minted token 1");
+
+  const balance = await contract.balanceOf(addr1.address);
+  console.log(balance)
+
+}
+
+async function TBA() {
+  let contract;
+  let owner;
+  let addr1;
+  let addr2;
+
+  // Get the signers from ethers
+  [owner, addr1, addr2] = await ethers.getSigners();
+
+  // Deploy the contract
+  const RegistryContract = await ethers.getContractFactory("ERC6551Registry");
+  const regristryContract = await RegistryContract.deploy();
+  const registryContractAddress = await regristryContract.getAddress()
+  console.log("Registry Contract deployed to address:", registryContractAddress);
+
+  const AccountContract = await ethers.getContractFactory("ERC6551Account");
+  const accountContract = await AccountContract.deploy();
+  const accountContractAddress = await accountContract.getAddress()
+  console.log("Account Contract deployed to address:", accountContractAddress);
+
+}
+
+TBA()
